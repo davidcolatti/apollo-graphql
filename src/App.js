5fb46e3f29c778b13437149be1522966fe411a74
DavidColatti <davidcolatti@gmail.com>
2020-06-18 14:50:05 -0400

    feat: first graphql query

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React from "react";
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
+import gql from "graphql-tag";
 
 const client = new ApolloClient({
   uri: "https://api-us-east-1.graphcms.com/v2/ckbl38xy2061c01z0fu8tfp9b/master",
 });
+
+const testQuery = gql`
+  {
+    posts {
+      id
+      title
+      body
+    }
+  }
+`;
+
+client
+  .query({
+    query: testQuery,
+  })
+  .then((res) => console.log(res));
 
 function App() {
   return (
