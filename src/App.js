466a2ec69e5c1e01fb866c21ae437035185fcf11
DavidColatti <davidcolatti@gmail.com>
2020-06-18 15:16:13 -0400

    feat: query component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import ApolloClient from "apollo-boost";
-import { ApolloProvider } from "react-apollo";
+import { ApolloProvider, Query } from "react-apollo";
 import gql from "graphql-tag";
 
+// Connecting site to the GraphQl API
 const client = new ApolloClient({
   uri: "https://api-us-east-1.graphcms.com/v2/ckbl38xy2061c01z0fu8tfp9b/master",
 });
 
-const testQuery = gql`
+// Writing a query
+const POSTS_QUERY = gql`
   {
     posts {
       id
@@ -17,16 +19,26 @@ const testQuery = gql`
   }
 `;
 
-client
-  .query({
-    query: testQuery,
-  })
-  .then((res) => console.log(res));
+// Running a query outside of React
+// client
+//   .query({
+//     query: testQuery,
+//   })
+//   .then((res) => console.log(res));
 
+// Apollo Provider attached the client to our React app
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className="App">App</div>
+      <div className="App">
+        <Query query={POSTS_QUERY}>
+          {({ loading, data }) => {
+            if (loading) return "Loading...";
+            const { posts } = data;
+            return posts.map((post) => <h1>{post.title}</h1>);
+          }}
+        </Query>
+      </div>
     </ApolloProvider>
   );
 }
